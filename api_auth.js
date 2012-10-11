92710000c88b7051b3c04f514baabdf1fab4b954
John Webster <jlwebster@gmail.com>
2012-10-11 03:59:54 -0600

    Simplified exports

--- api_auth.js
+++ api_auth.js
@@ -2,49 +2,49 @@
 // author : John Webster
 // license : MIT
 // https://github.com/jlwebster/node_api_auth_client
-(typeof define !== "function" ? function($){ $(require, exports, module); } : define)(function(require, exports, module, undefined) {
-  var crypto = require('crypto');
-  var moment = require('moment');
+var crypto = require('crypto');
+var moment = require('moment');
 
-  exports.auth = function(access_id, secret) {
-    this.access_id = access_id;
-    this.secret = secret;
+var exports = module.exports;
 
-    this.sign_options = function(options, content_body) {
-      var content_type = options.headers['Content-Type'];
-      if (isEmpty(content_type))
-      {
-        // Default to json
-        content_type = options.headers['Content-Type'] = 'application/json';
-      }
+exports.auth = function(access_id, secret) {
+  this.access_id = access_id;
+  this.secret = secret;
 
-      var path = options.path;
-      if (isEmpty(path))
-      {
-        // Default to the host's root
-        path = options.path = '/'
-      }
+  this.sign_options = function(options, content_body) {
+    var content_type = options.headers['Content-Type'];
+    if (isEmpty(content_type))
+    {
+      // Default to json
+      content_type = options.headers['Content-Type'] = 'application/json';
+    }
 
-      var content_md5 = crypto.createHash('md5').update(content_body).digest('base64');
-      var date = moment().utc().format('ddd, DD MMM YYYY HH:mm:ss') + ' GMT';
+    var path = options.path;
+    if (isEmpty(path))
+    {
+      // Default to the host's root
+      path = options.path = '/'
+    }
 
-      var canonical_string = [content_type, content_md5, path, date].join();
+    var content_md5 = crypto.createHash('md5').update(content_body).digest('base64');
+    var date = moment().utc().format('ddd, DD MMM YYYY HH:mm:ss') + ' GMT';
 
-      var auth_header_value = 'APIAuth ' + this.access_id + ':' + crypto.createHmac('sha1', this.secret).update(canonical_string).digest('base64');
+    var canonical_string = [content_type, content_md5, path, date].join();
 
-      options.headers['Content-MD5'] = content_md5;
-      options.headers['DATE'] = date;
-      options.headers['Authorization'] = auth_header_value;
+    var auth_header_value = 'APIAuth ' + this.access_id + ':' + crypto.createHmac('sha1', this.secret).update(canonical_string).digest('base64');
 
-      return options;
-    };
+    options.headers['Content-MD5'] = content_md5;
+    options.headers['DATE'] = date;
+    options.headers['Authorization'] = auth_header_value;
 
-    return this;
+    return options;
   };
 
-  function isEmpty(value)
-  {
-    return value === null || value === '' || typeof value === 'undefined';
-  }
-});
+  return this;
+};
+
+function isEmpty(value)
+{
+  return value === null || value === '' || typeof value === 'undefined';
+}
 
